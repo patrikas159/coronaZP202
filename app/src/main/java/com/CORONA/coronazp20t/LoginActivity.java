9b587d65507d943a6c00f9c18adeb84018fc8019
patrikas159 <patrikas156>
2020-10-15 15:38:03 +0300

    padaryta

--- app/src/main/java/com/CORONA/coronazp20t/LoginActivity.java
+++ app/src/main/java/com/CORONA/coronazp20t/LoginActivity.java
@@ -1,13 +1,37 @@
-package com.corona.coronazp20-2;
+package com.CORONA.coronazp20t;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.CORONA.coronazp20_2.SearchActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        super.onCreate(savedInstanceState);//atudaromas langas
+        setContentView(R.layout.activity_login);//pridedamas vaizdas
+        //kodas rasomas nuo cia
+        Button login=findViewById(R.id.Login);//susiejamas vaizde esantis elementas su kodu
+        final EditText username=findViewById(R.id.username);
+        final EditText password=findViewById(R.id.password);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //cia rasomas kodas kuris vykdomas paspaudus mygtuka
+                Toast.makeText(LoginActivity.this, "Prisijungimo vardas yra:"+
+                        username.getText().toString() + "\n" + "slaptazodis:" +
+                        password.getText().toString(), Toast.LENGTH_LONG).show();
+                //ketinimas pereiti i paieskos langa                is kur               i kur
+                Intent goToSearchActivity=new Intent(LoginActivity.this, SearchActivity.class);
+                startActivity(goToSearchActivity);
+            }
+        });
     }
 }
