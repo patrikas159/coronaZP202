0fe1ceece223658295359787a36a7f5be7837249
patrikas159 <71067431+patrikas159@users.noreply.github.com>
2020-11-05 17:30:17 +0200

    Add files via upload

---
+++ coronaZP202/app/src/main/java/com/CORONA/coronazp20_2/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.CORONA.coronazp20_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.CORONA.coronazp20t.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
